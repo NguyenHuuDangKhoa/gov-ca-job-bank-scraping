7799ec6399f95c7d9860fed80096e17e28b753b1
NguyenHuuDangKhoa <nhdkhoa1994@gmail.com>
2022-06-24 21:23:24 -0600

    Script is now able to scrape multiple postings (will be updated to OOP Class in next commit)

--- main.py
+++ main.py
@@ -11,83 +11,140 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 import selenium.common.exceptions as SeleniumException
 
+BASE_URL = "https://www.jobbank.gc.ca/jobsearch/jobsearch?dkw=ethnic&sort=M&fprov=AB&fsrc=16"
+
 os.environ["PATH"] += r"C:\Document\Python\selenium_driver"
 options = Options()
 options.add_experimental_option("detach", True)
+options.add_argument("--start-maximized")
 driver = webdriver.Chrome(options=options)
-# driver.get("https://www.jobbank.gc.ca/jobsearch/jobsearch?searchstring=&locationstring=Alberta&sort=M")
-driver.get("https://www.jobbank.gc.ca/jobsearch/jobposting/36466882?source=searchresults")
+
+driver.get(BASE_URL)
 driver.implicitly_wait(10)
-job_title = driver.find_element(By.CSS_SELECTOR, "span[property='title']").text
-posted_date = driver.find_element(By.CSS_SELECTOR, "span[property='datePosted']").text.replace("Posted on ", "")
-employer_name = driver.find_element(By.CSS_SELECTOR, "span[property='hiringOrganization']").text
-job_location = driver.find_element(By.CSS_SELECTOR, "span[class='noc-location']").text
-city = driver.find_element(By.CSS_SELECTOR, "span[property='addressLocality']").text
-province = driver.find_element(By.CSS_SELECTOR, "span[property='addressRegion']").text
-try:
-    street = driver.find_element(By.CSS_SELECTOR, "span[property='streetAddress']").text
-except SeleniumException.NoSuchElementException:
-    street = "N/A"
-try:
-    postal_code = driver.find_element(By.CSS_SELECTOR, "span[property='postalCode']").text
-except SeleniumException.NoSuchElementException:
-    postal_code = "N/A"
 
-job_requirement = driver.find_element(By.CSS_SELECTOR, "div.job-posting-detail-requirements ").text
-try:
-    print(job_requirement[job_requirement.index("Other benefits"):])
-    print(job_requirement[:job_requirement.index("Other benefits")])
-    benefits = job_requirement[job_requirement.index("Other benefits"):]
-    job_requirement = job_requirement[:job_requirement.index("Other benefits")]
-except ValueError:
-    benefits = "N/A"
+there_more = True
+show_more_button = driver.find_element(By.CSS_SELECTOR, "button[id='moreresultbutton']")
+show_more_button.click()
+
+while there_more:
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR,  "button[id='moreresultbutton']"))
+        ).click()
+    except SeleniumException.TimeoutException:
+        print("There's no more result")
+        there_more = False
 
 
-# salary_amount = soup.find("span", property="baseSalary", class_="attribute-value").getText()\
-#     .removeprefix("$").replace("HOUR", "") + " for" + soup.find("span", property="workHours").getText()
-# job_requirement = soup.find("div", property="skills").find_all("dd")
-salary_amount = driver.find_element(By.CSS_SELECTOR, "span[property='value']").text\
-    .replace("HOUR", "").replace("hourly", "").rstrip("\n")
-employment_type = driver.find_element(By.CSS_SELECTOR, "span[property='employmentType']").text
-work_hour = driver.find_element(By.CSS_SELECTOR, "span[property='workHours']").text
+articles = driver.find_elements(By.CSS_SELECTOR, "div[id='ajaxupdateform:result_block'] > article > a")
+url_list = []
+for article in articles:
+    url_list.append(article.get_attribute("href"))
+print(len(url_list))
+job_postings = {}
+job_posting_index = 1
+for url in url_list:
+    print("\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+    print(f"{job_posting_index} begin!")
+    driver.get(url)
+    driver.implicitly_wait(10)
+    job_title = driver.find_element(By.CSS_SELECTOR, "span[property='title']").text
+    posted_date = driver.find_element(By.CSS_SELECTOR, "span[property='datePosted']").text.replace("Posted on ", "")
+    employer_name = driver.find_element(By.CSS_SELECTOR, "span[property='hiringOrganization']").text
+    job_location = driver.find_element(By.CSS_SELECTOR, "span[class='noc-location']").text
+    city = driver.find_element(By.CSS_SELECTOR, "span[property='addressLocality']").text
+    province = driver.find_element(By.CSS_SELECTOR, "span[property='addressRegion']").text
+    try:
+        street = driver.find_element(By.CSS_SELECTOR, "span[property='streetAddress']").text
+    except SeleniumException.NoSuchElementException:
+        street = "N/A"
+    try:
+        postal_code = driver.find_element(By.CSS_SELECTOR, "span[property='postalCode']").text
+    except SeleniumException.NoSuchElementException:
+        postal_code = "N/A"
 
-# We need to scrape the start date but its value is inside a span tag without any class name,or, id, or attributes
-# I notice that tha span tag we need is inside an item list inside an unordered list
-# There are either 7 or 9 list items
-# If there are 7 list items then start date would be in list item 5
-# If there are 9 list items then start date would be in list item 4
-# The case for scraping vacancy information and job id are similar
-list_items = driver.find_elements(By.CSS_SELECTOR, "ul.job-posting-brief.colcount-lg-2 > li")
-if len(list_items) == 9:
-    start_date = list_items[4].text.replace("Start date", "").lstrip("\n")
-    vacancy = list_items[6].text.replace("vacancies", "").lstrip("\n")
-    job_id = list_items[-1].text.replace("Source", "").replace("Job Bank #", "").lstrip("\n")
-elif len(list_items) == 7:
-    start_date = list_items[3].text.replace("Start date", "").lstrip("\n")
-    vacancy = list_items[4].text.replace("vacancies", "").lstrip("\n")
-    job_id = list_items[-1].text.replace("Source", "").replace("Job Bank #", "").lstrip("\n")
-else:
-    start_date = "N/A"
-    vacancy = "N/A"
-    job_id = "N/A"
+    job_requirement = driver.find_element(By.CSS_SELECTOR, "div.job-posting-detail-requirements ").text
+    try:
+        print(job_requirement[job_requirement.index("Other benefits"):])
+        print(job_requirement[:job_requirement.index("Other benefits")])
+        benefits = job_requirement[job_requirement.index("Other benefits"):]
+        job_requirement = job_requirement[:job_requirement.index("Other benefits")]
+    except ValueError:
+        benefits = "N/A"
 
-print(job_title)
-print(posted_date)
-print(employer_name)
-print(job_location)
-print(street)
-print(city)
-print(province)
-print(postal_code)
-print(salary_amount)
-print(employment_type)
-print(work_hour)
-print(start_date)
-print(vacancy)
-print(job_id)
-print(job_requirement)
-print(benefits)
+    # salary_amount = soup.find("span", property="baseSalary", class_="attribute-value").getText()\
+    #     .removeprefix("$").replace("HOUR", "") + " for" + soup.find("span", property="workHours").getText()
+    # job_requirement = soup.find("div", property="skills").find_all("dd")
+    salary_amount = driver.find_element(By.CSS_SELECTOR, "span[property='value']").text \
+        .replace("HOUR", "").replace("hourly", "").rstrip("\n")
+    employment_type = driver.find_element(By.CSS_SELECTOR, "span[property='employmentType']").text
+    work_hour = driver.find_element(By.CSS_SELECTOR, "span[property='workHours']").text
 
-print("test")
+    # We need to scrape the start date but its value is inside a span tag without any class name,or, id, or attributes
+    # I notice that tha span tag we need is inside an item list inside an unordered list
+    # There are either 7 or 9 list items
+    # If there are 7 list items then start date would be in list item 5
+    # If there are 9 list items then start date would be in list item 4
+    # The case for scraping vacancy information and job id are similar
+    list_items = driver.find_elements(By.CSS_SELECTOR, "ul.job-posting-brief.colcount-lg-2 > li")
+    if len(list_items) == 9:
+        start_date = list_items[4].text.replace("Start date", "").lstrip("\n")
+        vacancy = list_items[6].text.replace("vacancies", "").lstrip("\n")
+        job_id = list_items[-1].text.replace("Source", "").replace("Job Bank #", "").lstrip("\n")
+    elif len(list_items) == 7:
+        start_date = list_items[3].text.replace("Start date", "").lstrip("\n")
+        vacancy = list_items[4].text.replace("vacancies", "").lstrip("\n")
+        job_id = list_items[-1].text.replace("Source", "").replace("Job Bank #", "").lstrip("\n")
+    else:
+        start_date = "N/A"
+        vacancy = "N/A"
+        job_id = "N/A"
+
+    print(job_title)
+    print(posted_date)
+    print(employer_name)
+    print(job_location)
+    print(street)
+    print(city)
+    print(province)
+    print(postal_code)
+    print(salary_amount)
+    print(employment_type)
+    print(work_hour)
+    print(start_date)
+    print(vacancy)
+    print(job_id)
+    print(job_requirement)
+    print(benefits)
+    print(f"{job_posting_index} finished!")
+
+    job_postings[job_posting_index] = {
+        "source_url": url,
+        "job_title": job_title,
+        "posted_date": posted_date,
+        "employer_name": employer_name,
+        "job_location": job_location,
+        "street": street,
+        "city": city,
+        "province": province,
+        "postal_code": postal_code,
+        "salary_amount": salary_amount,
+        "employment_type": employment_type,
+        "work_hour": work_hour,
+        "start_date": start_date,
+        "vacancy": vacancy,
+        "job_id": job_id,
+        "job_requirement": job_requirement,
+        "benefits": benefits,
+    }
+
+    job_posting_index += 1
+    if job_posting_index > 3:
+        break
+with open("job_postings.json", "w") as file:
+    json.dump(job_postings, file)
+
+
+
 
 
